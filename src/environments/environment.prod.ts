6c8281f9a2bfb4ceccc1a36a4a4009ebd068e55b
luismiguel0529 <l.miguel0529@hotmail.com>
2018-09-16 12:40:42 -0500

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,3 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyD9HcGZBbnAUg3pMXDDnK3_Px7MwvAsCa8",
-    authDomain: "fir-f9427.firebaseapp.com",
-    databaseURL: "https://fir-f9427.firebaseio.com",
-    projectId: "fir-f9427",
-    storageBucket: "fir-f9427.appspot.com",
-    messagingSenderId: "513087884924"
-
-
-  }
+  production: true
 };
